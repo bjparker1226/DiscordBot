517a411c960839638ce735732674a53c7d3ab785
Benjamin <benparker1226@gmail.com>
2023-07-11 09:55:50 -0400

    feat: set up client and add placeholder command

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+
 def main():
-    print("Hello World")
+    client.run('ODM3NTEyOTc3NDM4ODY3NDY3.GnKoaT.IWxj5vbTLqYIXEk5V4t7xL36Kywg1F4AZi7jcE')
 
 
 if __name__ == "__main__":
